f845cf57c0d285582a2097ba5f46488cc25bbdec
Nathan Bagge <natbag98@kingshigh.school.nz>
2024-06-21 19:19:55 +1200

    Added mouse interaction
    Added ability to check for mouse clicks and collisions to Input.

--- input.py
+++ input.py
@@ -2,6 +2,11 @@ import pygame
 
 
 class Input:
+    MOUSE_BUTTONS = {
+        'left': 0,
+        'middle': 1,
+        'right': 2
+    }
 
     def __init__(self):
         self.keys = {
@@ -10,9 +15,43 @@ class Input:
             if attr.startswith('K_')
         }
 
+        self.mouse_pos = pygame.mouse.get_pos()
+        self.mouse = {
+            key: MouseButton(self.MOUSE_BUTTONS[key], self)
+            for key in self.MOUSE_BUTTONS
+        }
+
     def update(self):
         keys = pygame.key.get_pressed()
         [key.update(keys) for key in self.keys.values()]
+
+        self.mouse_pos = pygame.mouse.get_pos()
+
+
+class MouseButton:
+
+    def __init__(self, button, input):
+        self.button = button
+        self.input = input
+        self.clicked = False
+        self.held = False
+        self.released = False
+
+    def interact(self, rect, click_type):
+        return rect.collide_point(self.input.mouse_pos) and self.__dict__[click_type]
+
+    def update(self):
+        if pygame.mouse.get_pressed()[self.button]:
+            if self.clicked:
+                self.clicked = False
+
+            if not self.held:
+                self.clicked = True
+            self.held = True
+        else:
+            if self.held:
+                self.released = True
+                self.held = False
 
 
 class Key:
